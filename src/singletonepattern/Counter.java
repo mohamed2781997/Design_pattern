f082c5a7b187ced51fb292ec5fe2609943cd6db4
magdy <mohammed2781997@gmail.com>
2023-09-26 19:39:31 +0300

    single tone pattern

---
+++ src/singletonepattern/Counter.java
@@ -0,0 +1,24 @@
+package singletonepattern;
+
+public class Counter {
+
+    public int count = 0;
+    private static Counter instance = null;
+
+    private Counter() {
+
+    }
+
+    public static Counter getinstance() {
+        if(instance==null){
+        instance=new Counter();
+        }
+        return instance;
+    }
+
+    public void addcount() {
+        count++;
+
+    }
+
+}
---
+++ src/singletonepattern/SingleTonePattern.java
@@ -0,0 +1,23 @@
+
+package singletonepattern;
+
+
+public class SingleTonePattern {
+
+  
+    public static void main(String[] args) {
+
+                Counter c1=Counter.getinstance();
+                Counter c2=Counter.getinstance();
+                c1.addcount();
+                c2.addcount();
+              System.out.println("Counter 1 :"+c1.count);
+              System.out.println("Conuter 2 :"+c2.count);
+
+              
+              c1.addcount();
+              System.out.println("Counter 1 :"+c1.count);
+              System.out.println("Conuter 2 :"+c2.count);
+    }
+    
+}
